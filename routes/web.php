ad33a7331cbcc7b4de97c4ecb6ea76b22467efbe
Muraoka <ym.animator@gmail.com>
2021-12-05 22:22:53 +0900

    製品ページのルーティング

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', HomeController::class)->name('home.index');
+Route::get('/products', [ProductController::class, 'index'])
+->name('product.index');
+Route::get('/products/{id}', [ProductController::class, 'show'])
+->name('product.show');
