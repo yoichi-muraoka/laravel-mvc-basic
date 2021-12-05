dfd21e894a21e04fc42a6882bfe37559f70d3415
Muraoka <ym.animator@gmail.com>
2021-12-05 22:33:50 +0900

    お問い合わせページのルーティング

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -16,7 +17,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', HomeController::class)->name('home.index');
+
 Route::get('/products', [ProductController::class, 'index'])
 ->name('product.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])
 ->name('product.show');
+
+Route::get('/contact', [ContactController::class, 'index'])
+->name('contact.index');
+Route::post('/contact', [ContactController::class, 'indexPost'])
+->name('contact.post');
+Route::get('/contact/done', [ContactController::class, 'done'])
+->name('contact.done');
