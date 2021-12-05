9084ab69db0f050e420aa7c575fecb31d0a22fe9
Muraoka <ym.animator@gmail.com>
2021-12-05 22:31:13 +0900

    問い合わせ内容の受付、バリデーション、入力内容を一時保持して完了ページにリダイレクト

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContactRequest;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -9,5 +10,11 @@ class ContactController extends Controller
     public function index()
     {
         return view('contact.index');
+    }
+    public function indexPost(ContactRequest $request)
+    {
+        $input = $request->validated();
+        return redirect()->route('contact.done')
+               ->with(['input' => $input]);
     }
 }
